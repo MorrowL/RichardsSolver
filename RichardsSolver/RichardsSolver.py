8c799689fcddc02407cdc6f6119568bb43adf4fe
LiamMorrow <liam.morrow@anu.edu.au>
2025-02-08 17:58:37 +1100

    Improvements

--- RichardsSolver/RichardsSolver.py
+++ RichardsSolver/RichardsSolver.py
@@ -1,64 +1,66 @@
-def RichardsSolver( h0, V, v, mesh, solverParameters, setBoundaryConditions, C, K, moistureContent ):
+def RichardsSolver( h0, V, v, mesh, solverParameters, modelParameters, timeParameters, setBoundaryConditions ):
 
-    from gadopt import Measure, Function, DirichletBC, dx, VTKFile, CheckpointFile, Constant, interpolate, assemble, sqrt, SpatialCoordinate
     import firedrake as fd
     import numpy as np
+    import ufl
     from ProblemDefinition import ProblemDefinitionNonlinear, ProblemDefinitionLinear
+    from modelTypes import relativePermeability, moistureContent
     from timeStepper import chooseTimeStep
     from scipy.io import savemat
 
-    dx = Measure("dx", domain=mesh, metadata={"quadrature_degree": 3})
-    ds = Measure("ds", domain=mesh, metadata={"quadrature_degree": 3})
+    dx = fd.Measure("dx", domain=mesh, metadata={"quadrature_degree": 3})
+    ds = fd.Measure("ds", domain=mesh, metadata={"quadrature_degree": 3})
     dimen = mesh.topological_dimension()
-    x     = SpatialCoordinate(mesh)
+    x     = fd.SpatialCoordinate(mesh)
 
     # Do some unpacking
-    finalTime = solverParameters["finalTime"]
-    maxdt     = solverParameters["maxdt"]
-    mindt     = solverParameters["mindt"]
-    imexParameter = solverParameters["imexParameter"]
+    finalTime     = timeParameters["finalTime"]
+    maxdt         = solverParameters["maxdt"]
+    mindt         = solverParameters["mindt"]
 
-    h     = Function(V, name="Pressure Head");         h.assign(h0)
-    hOld  = Function(V, name="PressureHeadOld");       hOld.assign(h0)
-    hStar = Function(V, name="ApproximateSolution");   hStar.assign(h0)
-    q     = Function(V, name="ApproximateSolution");   q.interpolate( -K(hOld, x)*(hOld.dx(0) - 1) )
-    theta = Function(V, name = "Moisture Conent")
+    h     = fd.Function(V, name="Pressure Head");         h.assign(h0)
+    hOld  = fd.Function(V, name="PressureHeadOld");       hOld.assign(h0)
+    hStar = fd.Function(V, name="ApproximateSolution");   hStar.assign(h0)
+    q     = fd.Function(V, name="ApproximateSolution");   q.interpolate( -relativePermeability(modelParameters, hOld, x, 0)*(hOld.dx(0) - 1) )
+    theta = fd.Function(V, name = "Moisture Conent")
 
-    DiffX = Constant( solverParameters["domainDepth"]*sqrt(assemble((q)**2 * dx)) )
-    timeStep = mindt; dt = Constant(timeStep)
+    DiffX = fd.Constant( solverParameters["Regularisation"] )
+    timeStep = timeParameters["timeStepSize"]; 
+    dt = fd.Constant(timeStep)
     currentTime = 0
     timeConstant = fd.Constant(currentTime)
     iterations = 0
 
-    solverRichardsNonlinear = ProblemDefinitionNonlinear( h, hOld, DiffX, timeConstant, dt, v, V, imexParameter, K, C, setBoundaryConditions, mesh, dx, ds )
+    solverRichardsNonlinear = ProblemDefinitionNonlinear( h, hOld, DiffX, timeConstant, dt, v, V, timeParameters['timeIntegration'], modelParameters, setBoundaryConditions, mesh, dx, ds )
 
     # Save the solution
-    tStore = np.zeros(solverParameters["numberPlots"]+2)
-    xStore = np.linspace(0, solverParameters["domainWidth"], solverParameters["nodesWidth"])
-    zStore = np.linspace(0, solverParameters["domainDepth"], solverParameters["nodesDepth"])
-    hStore = np.zeros((solverParameters["nodesWidth"], solverParameters["nodesDepth"], solverParameters["numberPlots"]+2))
-    thetaStore = np.zeros((solverParameters["nodesWidth"], solverParameters["nodesDepth"], solverParameters["numberPlots"]+2))
+    tStore = np.zeros(solverParameters["numberPlots"]+1)
+
+    if dimen == 1:
+
+        xStore = 0
+        zStore = np.linspace(0, solverParameters["domainDepth"], solverParameters["nodesDepth"])
+        hStore = np.zeros((solverParameters["nodesDepth"], solverParameters["numberPlots"]+1))
+        thetaStore = np.zeros((solverParameters["nodesDepth"], solverParameters["numberPlots"]+1))
+
+    elif dimen == 2:
+
+        xStore = np.linspace(0, solverParameters["domainWidth"], solverParameters["nodesWidth"])
+        zStore = np.linspace(0, solverParameters["domainDepth"], solverParameters["nodesDepth"])
+        hStore = np.zeros((solverParameters["nodesWidth"], solverParameters["nodesDepth"], solverParameters["numberPlots"]+1))
+        thetaStore = np.zeros((solverParameters["nodesWidth"], solverParameters["nodesDepth"], solverParameters["numberPlots"]+1))
+
+    else:
+
+        outfile = fd.VTKFile(solverParameters["fileName"])
     
     tNext = 0
     tInterval = finalTime / solverParameters["numberPlots"]
     plotIdx = 0
-    trig = 0
 
-    outfile = VTKFile("output.pvd")
+    while currentTime <= finalTime:
 
-    while currentTime < finalTime:
-
-        theta.interpolate(moistureContent(h, x))
-
-        # Solve the system
-        hOld.assign(h); hStar.assign(h)
-        if solverParameters["solverType"] == "Newton":
-            solverRichardsNonlinear.solve()
-        elif solverParameters["solverType"] == "Picard":
-            solverRichardsLinear.solve()
-
-        currentTime = np.round(currentTime + timeStep, 8)
-        iterations += 1
+        theta.interpolate(moistureContent(modelParameters, h, x, currentTime))
 
         # Save the solution
         if currentTime >= tNext:
@@ -66,38 +68,50 @@ def RichardsSolver( h0, V, v, mesh, solverParameters, setBoundaryConditions, C, 
             print("Time ", currentTime)
 
             tStore[plotIdx] = currentTime
-            outfile.write(h, theta, time=currentTime)
-            for X in range(len(xStore)):
-                for Z in range(len(zStore)):
-                    hStore[X, Z, plotIdx] = np.array( h.at(xStore[X], zStore[Z], dont_raise=True) )
-                    thetaStore[X, Z, plotIdx] = np.array( theta.at(xStore[X], zStore[Z], dont_raise=True) )
+
+            if dimen == 3:
+
+                outfile.write(h, theta, time=currentTime)
+
+            else:
+
+                if dimen == 1:
+
+                    for Z in range(len(zStore)):
+                        hStore[Z, plotIdx] = np.array( h.at(zStore[Z], dont_raise=True) )
+                        thetaStore[Z, plotIdx] = np.array( theta.at( zStore[Z], dont_raise=True) )
+
+                elif dimen == 2:
+
+                    for X in range(len(xStore)):
+                        for Z in range(len(zStore)):
+                            hStore[X, Z, plotIdx] = np.array( h.at(xStore[X], zStore[Z], dont_raise=True) )
+                            thetaStore[X, Z, plotIdx] = np.array( theta.at(xStore[X], zStore[Z], dont_raise=True) )
+
+
+                saveVariables = {'tStore' : tStore, 'z' : zStore, 'x' : xStore,  'pressureHead' : hStore, 'moistureContent' : thetaStore}
+                savemat(solverParameters["fileName"], saveVariables)
 
             plotIdx += 1
             tNext = tNext + tInterval
 
-            saveVariables = {'tStore' : tStore, 'z' : zStore, 'x' : xStore,  'pressureHead' : hStore, 'moistureContent' : thetaStore}
-            savemat(solverParameters["fileName"], saveVariables)
+        # Solve the system
+        hOld.assign(h); hStar.assign(h);
+        solverRichardsNonlinear.solve()
+        if timeParameters['timeIntegration'] == 'modifiedEuler':
+            hOld.assign( h )
+            solverRichardsNonlinear.solve()
+            h.assign( 0.5*(h + hStar))
 
-            # Update timestep
-        if iterations % 5 == 0:
-
-            timeStep = chooseTimeStep( h, hOld, timeStep, solverParameters, dx )
-            dt.assign(timeStep)
-
-            timeConstant.assign(round(currentTime))
-
-            q.interpolate( -K(hOld, x)*(hOld.dx(dimen-1) - 1) )
-            DiffX.assign( solverParameters["Regularisation"]*sqrt(assemble((q)**2 * dx)) )
+        currentTime = np.round(currentTime + timeStep, 8)
+    
+        iterations += 1
+        timeStep = timeParameters["timeStepSize"]
+        dt.assign( timeStep ) 
+        timeConstant.assign( currentTime )
 
         if (currentTime + timeStep) >= tNext:
             timeStep = tNext - currentTime
             timeStep = np.minimum(timeStep, maxdt)
             timeStep = np.maximum(timeStep, 1e-05)
             dt.assign(timeStep)
-
-    for X in range(len(xStore)):
-        for Z in range(len(zStore)):
-            hStore[X, Z, plotIdx]     = np.array( h.at(xStore[X], zStore[Z], dont_raise=True) )
-            thetaStore[X, Z, plotIdx] = np.array( theta.at(xStore[X], zStore[Z], dont_raise=True) )
-
-    return h, tStore, hStore, thetaStore, zStore, xStore
---
+++ RichardsSolver/modelTypes.py
@@ -0,0 +1,99 @@
+def moistureContent( modelParameters, h, x, time):
+
+    import firedrake as fd
+
+    if modelParameters["modelType"] == "Haverkamp":
+
+        alpha  = modelParameters["alpha"]
+        beta   = modelParameters["beta"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+
+        theta = thetaR + alpha*(thetaS - thetaR) / (alpha + abs(h)**beta) ;
+    
+    elif modelParameters["modelType"] == "VanGenuchten":
+
+        alpha  = modelParameters["alpha"]
+        n      = modelParameters["n"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+        m = 1 - 1/n
+
+        theta = thetaR + (thetaS - thetaR) / ( (1 + abs(alpha*h)**n)**m )
+
+    elif modelParameters["modelType"] == "exponential":
+
+        alpha  = modelParameters["alpha"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+
+        theta = thetaR + (thetaS - thetaR) * fd.exp(h * alpha) 
+
+    else:
+
+        print("Model type not recognised")
+
+    return theta
+
+def relativePermeability( modelParameters, h, x, time):
+
+    import firedrake as fd
+  
+    if modelParameters["modelType"] == "Haverkamp":
+
+        A     = modelParameters["A"]
+        gamma = modelParameters["gamma"]
+        Ks    = modelParameters["Ks"]
+
+        K =  Ks*A / ( A + pow(abs(h), gamma) )
+
+    elif modelParameters["modelType"] == "VanGenuchten":
+
+        alpha  = modelParameters["alpha"]
+        n      = modelParameters["n"]
+        Ks     = modelParameters["Ks"]
+        m = 1 - 1/n
+
+        K =  Ks * (1 - (alpha*abs(h))**(n-1)* (1 + pow(alpha*abs(h), n))**(-m))**2 / (( 1 + pow(alpha*abs(h), n) )**(m/2))
+
+    elif modelParameters["modelType"] == "exponential":
+
+        alpha  = modelParameters["alpha"]
+        Ks     = modelParameters["Ks"]
+
+        K = Ks*fd.exp(h*alpha)
+
+    return K
+
+def waterRetention( modelParameters, h, x, time):
+
+    import firedrake as fd
+  
+    if modelParameters["modelType"] == "Haverkamp":
+
+        alpha  = modelParameters["alpha"]
+        beta   = modelParameters["beta"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+
+        C = ( -fd.sign(h)*alpha*beta*(thetaS - thetaR)*pow(abs(h),beta-1) ) / ( pow(alpha + pow(abs(h),beta),2) )
+
+    elif modelParameters["modelType"] == "VanGenuchten":
+
+        alpha  = modelParameters["alpha"]
+        n      = modelParameters["n"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+        m = 1 - 1/n
+
+        C =  -(thetaS - thetaR) * n * m * h * (alpha**n) * (abs(h)**(n-2)) * (( alpha**n * abs(h)**n + 1)**(-m-1) ) 
+
+    elif modelParameters["modelType"] == "exponential":
+
+        alpha  = modelParameters["alpha"]
+        thetaR = modelParameters["thetaR"]
+        thetaS = modelParameters["thetaS"]
+
+        C = (thetaS - thetaR) * fd.exp(h * alpha) * alpha 
+
+    return C

d9c57e31349c2aa0c97f8b8bf0ea97ad76c7d693
Liam Morrow <52473947+MorrowL@users.noreply.github.com>
2025-12-09 16:11:19 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import sys
+
+script_args = sys.argv[1:]
+
+setup(name='RichardsSolver',
+      version='1.0',
+      author='Liam Morrow',
+      author_email='liam.morrow@anu.edu.au',
+      description='A numerical package for solving Richards equation',
+      long_description=open('README.rst').read(),
+      url='https://github.com/g-adopt/gwassess',
+      packages=['gwassess'],
+      install_requires=['numpy', 'scipy'],
+      keywords=['Richards equation', 'analytical solutions',
+                'groundwater flow', 'unsaturated flow', 'benchmarks'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering'],
+      script_args=script_args,
+      ext_package='gwassess',
+      )

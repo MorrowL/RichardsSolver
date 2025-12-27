e77b8ee4f43c05908deefdc0eb8e000bec64abbf
Liam Morrow <52473947+MorrowL@users.noreply.github.com>
2025-12-27 21:08:30 +1100

    Add files via upload

--- RichardsSolver/richards_equation.py
+++ RichardsSolver/richards_equation.py
@@ -8,31 +8,44 @@ from RichardsSolver.utilities import CombinedSurfaceMeasure
 
 class RichardsSolver(ABC):
 
-    def __init__(self, 
+    """
+    Base class for Richards equation solvers. 
+    Handles: 
+       - mesh and function space setup 
+       - facet/volume measures (extruded or not) 
+       - solver parameter selection 
+    """
+
+    def __init__(self,
                  V: fd.FunctionSpace,
                  W: fd.VectorFunctionSpace,
                  mesh: fd.mesh,
                  soil_curves: Dict,
                  bcs: Dict,
-                 solver_parameters='iterative', 
+                 solver_parameters='iterative',
                  time_integrator="BackwardEuler",
-                 quad_degree=5):
+                 quad_degree=0):
 
         self.mesh = mesh
         self.trial_space = V
-        self.q = fd.Function(W, name="VolumetricFlux")
-        self.quad_degree = quad_degree
-        self.v = fd.TestFunction(V)
+        self.test_function = fd.TestFunction(V)
+
         self.dimen = mesh.topological_dimension()
         self.n = fd.FacetNormal(mesh)
+
+        self.q = fd.Function(W, name="VolumetricFlux")
         self.time_integrator = time_integrator
         self.soil_curves = soil_curves
         self.bcs = bcs
 
+        if quad_degree == 0:
+            quad_degree = 2 * max(V.ufl_element().degree()) + 1
+        self.quad_degree = quad_degree
+
+        # Measures (extruded vs non-extruded)
         measure_kwargs = {"domain": self.mesh, "degree": quad_degree}
-        
         self.dx = fd.dx(**measure_kwargs)
-        
+
         if self.trial_space.extruded:
             self.ds = CombinedSurfaceMeasure(**measure_kwargs)
             self.dS = fd.dS_v(**measure_kwargs) + fd.dS_h(**measure_kwargs)
@@ -47,11 +60,13 @@ class RichardsSolver(ABC):
                 "pc_type": 'lu',
                 "pc_factor_mat_solver_type": "mumps"
                 }
+            
         elif solver_parameters == "iterative":
             self.solver_parameters = {
                 "mat_type": "aij",
                 "ksp_type": 'gmres',
                 "pc_type": 'bjacobi',
                 }
+            
         else:
             self.solver_parameters = solver_parameters
--- RichardsSolver/richards_solver.py
+++ RichardsSolver/richards_solver.py
@@ -4,22 +4,18 @@ import numpy as np
 import ufl
 
 """
-This model provides a solver solving solving Richards equation in pressure-head form:
-$$(C + S_sS) \frac{\parial h}{\partial t} = \nabla (K \grad(h + z))
+Richards equation solver in mixed form:
+
+    ∂θ/∂t = ∇·(K(h) ∇(h + z))
+
 where:
-- $h$ is the pressure head
-- $S_s$ is the specific storage coefficient
-- $S(h) = (\theta - \theta_r)/(\theta_s - \theta_r)$ is the effective saturation
-- $C(h) = d\theta/dh$ is the specific moisture capacity
-- $K(h)$ is the hydraulic conductivity
-- $z$ is the vertical coordinate (gravity term)
-This equation is expressed in variational (weak) form and expressied in the residual form $0 = F(h, v)$ where $v$ is the test function
-INPUTS:
-    h : solution at current time step to be solved for
-    h_hold : solution at previous time step
-    time : curent time
-    time_step : size of timestep
-    eq : dumb
+    h  = pressure head
+    S  = effective saturation
+    C  = specific moisture capacity
+    K  = hydraulic conductivity
+    z  = vertical coordinate
+
+The solver constructs the weak residual F(h, v) = 0 for a given test function v.
 """
 
 
@@ -44,10 +40,10 @@ def richardsSolver(h: fd.Function, h_old: fd.Function, time: fd.Constant, time_s
             hDiff = (h + h_old)/2
             K = (relative_conductivity(h)+relative_conductivity(h_old))/2
         case _:
-            raise ValueError("Temporal integration method not recognised")
+            raise ValueError(f"Unknown time integrator '{eq.time_integrator}'")
     K_old = relative_conductivity(h_old)
 
-    # Richards equation in variational residual form F = 0
+    # --- Residual ---
     F = mass_term(h, h_old, time_step, eq) \
         + gravity_advection(K, eq) \
         + diffusion_term(hDiff, K, K_old, eq)
@@ -57,8 +53,10 @@ def richardsSolver(h: fd.Function, h_old: fd.Function, time: fd.Constant, time_s
     # Jacobian not needed for SemiImplicit
     solver_parameters = eq.solver_parameters
 
-    solverRichardsNonlinear = fd.NonlinearVariationalSolver(problem,
-                                solver_parameters=solver_parameters)
+    solverRichardsNonlinear = fd.NonlinearVariationalSolver(
+                                problem,
+                                solver_parameters=solver_parameters
+                                )
 
     return solverRichardsNonlinear
 
@@ -70,27 +68,33 @@ def mass_term(h: fd.Function, h_old: fd.Function, time_step: fd.Constant, eq):
 
     if eq.time_integrator == "SemiImplicit":
         C = eq.soil_curves.water_retention(h_old)
-        F = fd.inner(C*(h - h_old)/time_step, eq.v) * eq.dx
+        F = fd.inner(C*(h - h_old)/time_step, eq.test_function) * eq.dx
     else:
-        F = fd.inner((theta_new - theta_old)/time_step, eq.v) * eq.dx
+        F = fd.inner((theta_new - theta_old)/time_step, eq.test_function) * eq.dx
 
     return F
 
 
 def diffusion_term(hDiff: fd.Function, K: fd.Function, K_old: fd.Function, eq):
 
+    v = eq.test_function
+    grad_v = fd.grad(v)
     bcs = eq.bcs
 
     # Volume integral
-    F = fd.inner(fd.grad(eq.v), K * fd.grad(hDiff)) * eq.dx
+    F = fd.inner(grad_v, K * fd.grad(h_mid)) * eq.dx
 
     # SIPG
     sigma = interior_penalty_factor(eq, shift=0)
     sigma_int = sigma * fd.avg(fd.FacetArea(eq.mesh) / fd.CellVolume(eq.mesh))
 
-    F += sigma_int*fd.inner(fd.jump(eq.v, eq.n), fd.avg(K_old) * fd.jump(hDiff, eq.n)) * eq.dS
-    F -= fd.inner(fd.avg(K_old * fd.grad(eq.v)), fd.jump(hDiff, eq.n)) * eq.dS
-    F -= fd.inner(fd.jump(eq.v, eq.n), fd.avg(K_old * fd.grad(hDiff))) * eq.dS
+    jump_v = fd.jump(v, eq.n) 
+    jump_h = fd.jump(h_mid, eq.n) 
+    avg_K  = fd.avg(K_old)
+
+    F += sigma_int * fd.inner(jump_v, avg_K * jump_h) * eq.dS 
+    F -= fd.inner(fd.avg(K_old * grad_v), jump_h) * eq.dS 
+    F -= fd.inner(jump_v, fd.avg(K_old * fd.grad(h_mid))) * eq.dS
 
     # Impose bcs within the weak formulation
     for bc_idx, bc_info in bcs.items():
@@ -99,33 +103,32 @@ def diffusion_term(hDiff: fd.Function, K: fd.Function, K_old: fd.Function, eq):
         boundaryValue = boundaryInfo[boundaryType]
         if boundaryType == 'h':
             sigma_ext = sigma * fd.FacetArea(eq.mesh) / fd.CellVolume(eq.mesh)
-            F += 2 * sigma_ext * eq.v * K * (hDiff - boundaryValue) * eq.ds(bc_idx)
-            F -= fd.inner(K * fd.grad(eq.v), eq.n) * (hDiff - boundaryValue) * eq.ds(bc_idx)
-            F -= fd.inner(eq.v * eq.n, K * fd.grad(hDiff)) * eq.ds(bc_idx)
+            diff = hDiff - boundaryValue
+
+            F += 2 * sigma_ext * v * K * diff * eq.ds(bc_idx)
+            F -= fd.inner(K * grad_v, eq.n) * diff * eq.ds(bc_idx)
+            F -= fd.inner(v * eq.n, K * fd.grad(hDiff)) * eq.ds(bc_idx)
         elif boundaryType == 'flux':
-            F -= boundaryValue * eq.v * eq.ds(bc_idx)
+            F -= boundaryValue * eq.test_function * eq.ds(bc_idx)
         else:
             raise ValueError("Unknown boundary type, must be 'h' or 'flux'")
 
     return F
 
 
-def gravity_advection(K: fd.Function, eq):
+def gravity_advection(K, eq): 
 
-    # Gravity driven volumetric flux
+    v = eq.test_function 
+    x = fd.SpatialCoordinate(eq.mesh) 
 
-    v = eq.v
-    x = fd.SpatialCoordinate(eq.mesh)
+    # Vertical unit vector
+    z = x[eq.dim - 1]
+    vertical = fd.grad(z)
 
-    nDown = fd.grad(x[eq.dimen-1])
+    q = K * vertical 
+    qn = 0.5 * (fd.dot(q, eq.n) + abs(fd.dot(q, eq.n))) 
 
-    q  = K * nDown
-    qn = 0.5*(fd.dot(q, eq.n) + abs(fd.dot(q, eq.n)))
-
-    # Main volume integral
-    F = fd.inner(q, fd.grad(eq.v))*eq.dx
-    
-    # Upwinding
-    F -= fd.jump(v)*(qn('+') - qn('-'))*eq.dS
+    F = fd.inner(q, fd.grad(v)) * eq.dx   # Main volume integral
+    F -= fd.jump(v) * (qn("+") - qn("-")) * eq.dS  # Upwinding
 
     return F

ae530f3eff59f6072fa90e0f890aff0df51bc3bb
Liam Morrow <52473947+MorrowL@users.noreply.github.com>
2026-01-21 17:19:33 +1100

    Update richards_equation.py

--- RichardsSolver/richards_equation.py
+++ RichardsSolver/richards_equation.py
@@ -22,8 +22,9 @@ class RichardsSolver(ABC):
                  mesh: fd.mesh,
                  soil_curves: Dict,
                  bcs: Dict,
-                 solver_parameters='iterative',
+                 solver_parameters='default',
                  time_integrator="BackwardEuler",
+                 source_term=0,
                  quad_degree=0):
 
         self.mesh = mesh
@@ -34,16 +35,24 @@ class RichardsSolver(ABC):
         self.n = fd.FacetNormal(mesh)
 
         self.q = fd.Function(W, name="VolumetricFlux")
-        self.time_integrator = time_integrator
+        self.h_star = fd.Function(V, name='ApproximateSolution')
+
+        accepted_solvers = ['BackwardEuler', 'CrankNicolson', 'Picard', 'ImplicitMidpoint', 'SemiImplicit']
+        if time_integrator in accepted_solvers:
+            self.time_integrator = time_integrator
+        else:
+            TypeError('Time Integrator not recognised')
         self.soil_curves = soil_curves
         self.bcs = bcs
+
+        self.source_term = source_term
 
         if quad_degree == 0:
             
             degree = V.ufl_element().degree()
             if not isinstance(degree, int):
                 degree = max(degree)
-            quad_degree = 2 * max(V.ufl_element().degree()) + 1
+            quad_degree = 2 * degree + 1
         self.quad_degree = quad_degree
 
         # Measures (extruded vs non-extruded)
@@ -57,20 +66,44 @@ class RichardsSolver(ABC):
             self.dS = fd.Measure("dS", domain=mesh, metadata={"quadrature_degree": quad_degree})
             self.ds = fd.Measure("ds", domain=mesh, metadata={"quadrature_degree": quad_degree})
 
-        if solver_parameters == "direct":
-            self.solver_parameters = {
-                "mat_type": "aij",
-                "ksp_type": 'preonly',
-                "pc_type": 'lu',
-                "pc_factor_mat_solver_type": "mumps"
-                }
-            
+        if solver_parameters == "default":
+            if mesh.topological_dimension() <= 2:
+                solver_parameters = 'direct'
+            else:
+                solver_parameters = 'iterative'
+
+        if solver_parameters == 'direct':
+            if time_integrator == 'SemiImplicit':
+                self.solver_parameters = {
+                    "mat_type": "aij",
+                    "ksp_type": 'preonly',
+                    "pc_type": 'lu',
+                    "pc_factor_mat_solver_type": "mumps",
+                    'snes_type': 'ksponly',
+                    }
+            else:
+                self.solver_parameters = {
+                    "mat_type": "aij",
+                    "ksp_type": 'preonly',
+                    "pc_type": 'lu',
+                    "pc_factor_mat_solver_type": "mumps",
+                    'snes_type': 'newtonls'
+                    }
         elif solver_parameters == "iterative":
-            self.solver_parameters = {
-                "mat_type": "aij",
-                "ksp_type": 'gmres',
-                "pc_type": 'bjacobi',
-                }
-            
+            if time_integrator == 'SemiImplicit':
+                self.solver_parameters = {
+                    "mat_type": "aij",
+                    "ksp_type": 'gmres',
+                    "pc_type": 'bjacobi',
+                    'snes_type': 'ksponly',
+                    }
+            else:
+                self.solver_parameters = {
+                    "mat_type": "aij",
+                    "ksp_type": 'gmres',
+                    "pc_type": 'bjacobi',
+                    'snes_type': 'newtonls'
+                    }
+        # User specified solver parameters
         else:
             self.solver_parameters = solver_parameters
